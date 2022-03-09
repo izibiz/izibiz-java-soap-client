fe5c233cc31dfed976d7ec0b33dfe0afe8b71169
Taha Dönük <rtdonuk@gmail.com>
2022-03-09 12:20:25 +0300

    initial

---
+++ izibiz-soap-java-client/main/java/com/edonusum/client/IzibizSoapJavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.edonusum.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzibizSoapJavaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IzibizSoapJavaClientApplication.class, args);
+    }
+
+}
---
+++ izibiz-soap-java-client/test/java/com/edonusum/client/IzibizSoapJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edonusum.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzibizSoapJavaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
